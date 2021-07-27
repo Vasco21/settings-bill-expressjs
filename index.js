303ac93c9353e4ebc9e2cf9feb63afe86825fd85
Vasco Eti <vascoeti123@gmail.com>
2021-07-27 17:19:09 +0200

    update

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+var exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const SettingsBill = require('./settings-bill');
+const moment = require('moment');
+const handlebarSetup = exphbs({
+    partialsDir: "./views/partials",
+    viewPath:  './views',
+    layoutsDir : './views/layouts'
+});
+
+
+const app = express();
+const settingsBill = SettingsBill();
+ 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebarSetup);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    if (settingsBill.values().warn === undefined ) {
+        res.render('index', {
+          settings: settingsBill.getSettings(),
+          totals: settingsBill.totals(),
+          globalTotal: 'globalTotal'
+        });
+      } 
+      else if (settingsBill.BothDanger()) {
+        res.render('index', {
+          settings: settingsBill.getSettings(),
+          totals: settingsBill.totals(),
+          globalTotal: 'globalTotal'
+        });
+      }
+       else if (settingsBill.hasReachedWarningLevel()) {
+        res.render('index', {
+          settings: settingsBill.getSettings(),
+          totals: settingsBill.totals(),
+          globalTotal: 'warning'
+        });
+      } 
+      else if (settingsBill.hasReachedCriticalLevel()) {
+        res.render('index', {
+          settings: settingsBill.getSettings(),
+          totals: settingsBill.totals(),
+          globalTotal: 'globalTotal' 
+        }); 
+    }
+});
+
+app.post('/settings', (req, res) => {
+    
+    settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel      
+    });
+    res.redirect('/');
+});
+
+app.post('/action', (req, res) => {
+    // capture the call type to add 
+    settingsBill.recordAction(req.body.actionType);
+    res.redirect('/')
+});
+
+app.get('/actions', (req, res) => {
+    res.render('actions', {actions : settingsBill.actions()});
+ 
+});
+
+app.get('/actions/:actionType', (req, res) => {
+    const actionType = req.params.actionType;
+    res.render('actions', {actions : settingsBill.actionsFor(actionType)});
+   
+});
+
+const PORT = process.env.PORT || 3011;
+
+app.listen(PORT, function () {
+    console.log("App started at port")
+});
--- settings-bill.js
+++ settings-bill.js
@@ -4,6 +4,10 @@ module.exports = function SettingsBill(){
     let warningLevel;
     let criticalLevel;
 
+    let combainTotals = 0;
+    const moment = require('moment');
+
+    let objList = [];
     let actionList = [];
 
     var setSettings = function (settings){
@@ -23,18 +27,31 @@ module.exports = function SettingsBill(){
     }
     var recordAction = function (action){
         let cost = 0;
+        if(grandTotal () < criticalLevel){
 
-        if (action === 'sms'){
-            cost = smsCost;
+            if (action === 'sms'){
+                cost = smsCost;
+            }
+            else if (action === 'call'){
+                cost = callCost;
+            }
+            if(action != undefined){
+                actionList.push({
+                    type: action,
+                    cost,
+                    timestamp : new Date()
+                });
+                objList.push({
+                    type: action,
+                    cost,
+                    timestamp : new Date()
+                });
+            }
+            for (let i = 0; i < objList.length; i++) {
+                let = timestamp = moment(objList[i].timestamp).format('mmmm do yyyy, h:mm:ss a')                ;
+                objList[i].timestamp = (moment(timestamp, 'mmmm do yyyy, h:mm:ss a').fromNow());
+            }
         }
-        else if (action === 'call'){
-            cost = callCost;
-        }
-        actionList.push({
-            type: action,
-            cost,
-            timestamp : new Date()
-        });
     }
 
     var actions = function (){
@@ -42,22 +59,43 @@ module.exports = function SettingsBill(){
     }
 
     var actionsFor = function(type){
-        return actionList.filter((action) => action.type === type)
+        const filter = [];
+        
+        for (let i = 0; i < actionList.length; i++){
+            const action = actionList[i];
+            if(action.type === type){
+                filter.push(action);
+            }
+            else if(type == 'total'){
+                filter.push(action);
+            }
+        }
+
+        return filter;
     }
 
     var getTotal = function(type){
-        return actionList.reduce((total, action) =>{
-            let val = action.type === type ? action.cost : 0;
-            return total + val;
-        }, 0);
+        let total = 0;
+        
+        for (let i = 0; i < actionList.length; i++) {
+            const action = actionList[i];
+            
+            if (action.type === type) {
+                total += action.cost;
+            }
+        }
+        return total;
     }
+    
     var grandTotal = function(){
-        return getTotal('sms') + getTotal('call');
+        let globalTotal = getTotal('sms') + getTotal('call')
+        combainTotals = Number(globalTotal).toFixed(2);
+        return Number(globalTotal).toFixed(2);
     }
 
     var totals = function(){
-        let smsTotal = getTotal('sms');
-        let callTotal = getTotal('call');
+        let smsTotal = getTotal('sms').toFixed(2);
+        let callTotal = getTotal('call').toFixed(2);
         return{
             smsTotal,
             callTotal,
@@ -66,14 +104,43 @@ module.exports = function SettingsBill(){
     }
     var hasReachedWarningLevel = function(){
         const total = grandTotal();
-        const reachedWarningLevel = total >= warningLevel && total < criticalLevel;
-        return reachedWarningLevel;
+
+        if (total + callCost >= warningLevel && total + callCost < criticalLevel) {
+            return true;
+        } else if (total + smsCost >= warningLevel && total + smsCost < criticalLevel) {
+            return true;
+        } else if (total >= warningLevel && total < criticalLevel) {
+            return true;
+        }
+        return false;
     }
 
     var hasReachedCriticalLevel = function() {
         const total = grandTotal();
-        return total >= criticalLevel;
+        if (total + callCost >= criticalLevel || total + smsCost >= criticalLevel) {
+            return true;
+        } else if (total >= criticalLevel) {
+            return true;
+        }
+        return false;  
     }
+
+    var  BothDanger =function() {
+        const total = grandTotal();
+        if (total < warningLevel && total < criticalLevel) {
+           return true; 
+        }
+        return false;
+    }
+
+    var  values = function(){
+        return {
+            warningLevel: warningLevel,
+            criticalLevel: criticalLevel,
+            combainTotals: combainTotals,
+        }
+    }
+
 
     return {
         setSettings,
@@ -82,7 +149,10 @@ module.exports = function SettingsBill(){
         actions,
         actionsFor,
         totals,
+        grandTotal,
         hasReachedWarningLevel,
-        hasReachedCriticalLevel
+        hasReachedCriticalLevel,
+        BothDanger,
+        values
     }
 }
